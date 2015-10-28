55c0e88349d39216fcb1361ee4f801e699ff87ec
Vasiliy Aboymov <aboymov@gmail.com>
2015-10-28 14:25:08 +0300

    terminator

--- create_links.sh
+++ create_links.sh
@@ -1,3 +1,4 @@
 ln -sf `pwd`/.i3status.conf ~/.i3status.conf
 ln -sf `pwd`/config ~/.i3/config
 ln -sf `pwd`/.xscreensaver ~/.xscreensaver
+ln -sf `pwd`/terminator/config ~/.config/terminator/config
