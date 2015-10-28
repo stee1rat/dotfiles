714f99528567f14dad1a2f5a01487b54793b3bf7
Vasiliy Aboymov <aboymov@gmail.com>
2015-10-28 21:55:59 +0300

    added .vimrc

---
+++ .vimrc
@@ -0,0 +1,7 @@
+set t_Co=256
+syntax on
+set background=dark
+color distinguished
+set expandtab
+set tabstop=3
+set shiftwidth=3
--- create_links.sh
+++ create_links.sh
@@ -2,3 +2,4 @@ ln -sf `pwd`/.i3status.conf ~/.i3status.conf
 ln -sf `pwd`/config ~/.i3/config
 ln -sf `pwd`/.xscreensaver ~/.xscreensaver
 ln -sf `pwd`/terminator/config ~/.config/terminator/config
+ln -sf `pwd`/.vimrc ~/.vimrc
